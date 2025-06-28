bbfaf6b045a158c23efe68add40ed301607d1cb1
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-06-28 15:39:26 +0300

    View shop #17

---
+++ frontend/src/pages/Shop.jsx
@@ -0,0 +1,8 @@
+export default function Shop() {
+    return (
+        <div style={{ padding: "100px", color: "white" }}>
+            <h1>🛒 Shop Page</h1>
+            <p>Aici poți vedea produsele disponibile.</p>
+        </div>
+    );
+}
