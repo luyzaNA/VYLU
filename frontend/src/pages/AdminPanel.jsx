90e17679bcfbc736f38aa2c4368bede4d6b2e2ee
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-06-28 15:40:40 +0300

    Add admin panel #62

---
+++ frontend/src/pages/AdminPanel.jsx
@@ -0,0 +1,8 @@
+export default function AdminPanel() {
+    return (
+        <div style={{ padding: "100px", color: "white" }}>
+            <h1>❓ ADMIN PAGE</h1>
+            <p>Ai întrebări? Suntem aici să te ajutăm.</p>
+        </div>
+    );
+}
