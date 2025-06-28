b3d87bb4b7e1974422cddd0d42f6d31f160e933c
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-06-28 15:31:05 +0300

    Add routing #6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home.jsx";
+import Help from "./pages/Help.jsx";
+import Shop from "./pages/Shop.jsx";
+import AdminPanel from "./pages/AdminPanel.jsx";
+import LoginPage from "./pages/Auth";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Profiles from "./pages/Profiles.jsx";
+import AddNewProfile from "./pages/AddNewProfile.jsx";
+import EditProfilePage from "./pages/EditProfile.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="app-container flex flex-col min-h-screen">
+                    <Navbar />
+                    <main className="flex-grow mt-24">
+                        <Routes>
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/help" element={<Help />} />
+                            <Route path="/shop" element={<Shop />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="" element={<Home />} />
+                            <Route
+                                path="/profiles"
+                                element={
+                                    <ProtectedRoute>
+                                        <Profiles />
+                                    </ProtectedRoute>
+                                }
+                            />
+                            <Route
+                                path="/add-profile"
+                                element={
+                                    <ProtectedRoute>
+                                        <AddNewProfile />
+                                    </ProtectedRoute>
+                                }
+                            />
+                            <Route
+                                path="/edit-profile/:id"
+                                element={
+                                    <ProtectedRoute>
+                                        <EditProfilePage />
+                                    </ProtectedRoute>
+                                }
+                            />
+                            <Route
+                                path="/admin"
+                                element={
+                                    <ProtectedRoute roles={["admin"]}>
+                                        <AdminPanel />
+                                    </ProtectedRoute>
+                                }
+                            />
+                        </Routes>
+                    </main>
+                    <Footer />
+                </div>
+            </Router>
+        </AuthProvider>
+    );
 }
 
-export default App
+export default App;
