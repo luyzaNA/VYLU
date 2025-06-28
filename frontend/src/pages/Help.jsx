ed1994a198e05cab84d142a6ff1bdb092a231914
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-06-28 15:40:22 +0300

    Add help section #61

---
+++ frontend/src/pages/Help.jsx
@@ -0,0 +1,8 @@
+export default function Help() {
+    return (
+        <div style={{ padding: "100px", color: "white" }}>
+            <h1>❓ Help Page</h1>
+            <p>Ai întrebări? Suntem aici să te ajutăm.</p>
+        </div>
+    );
+}
