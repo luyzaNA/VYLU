dc90a68d05b4acc14009674fe9ff044a4883ebe8
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-06-28 15:38:56 +0300

    Add home page #8

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div style={{ padding: "100px", color: "white" }}>
+            <h1>🏠 Home Page</h1>
+            <p>Bine ai venit pe pagina principală!</p>
+        </div>
+    );
+}
